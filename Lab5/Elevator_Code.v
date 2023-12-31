f0972f752da11e23a68142cea8475d271d5ce047
YoussefPr0jects <134124929+YoussefPr0jects@users.noreply.github.com>
2023-12-31 14:44:02 +0200

    Add files via upload

--- Lab5/Elevator_Code.v
+++ Lab5/Elevator_Code.v
@@ -1,103 +1,130 @@
 module Elevator_Code(
     input sys_clk,
     input sys_rst,
-    output reg rst_5,
+    input valid,
+    input [2:0] SW,
+    output reg rst_1,
     output reg rst_2,
-    output reg rst_8,
-    input flag_5s,
+    output reg rst_4,
+    input flag_1s,
     input flag_2s,
-    input flag_8s,
-    output reg Safeguard_status,
-    output reg Red_led,
-    output reg Green_led,
+    input flag_4s,
+    output reg Door_status,
+    output reg[2:0] current_floor,
     output reg [3:0] state
 );
-reg [3:0] counter = 0;
-parameter password1 = 123456;
-parameter password2 = 654321;
-parameter password3 = 111111;
-parameter password4 = 999999;
-parameter password5 = 000000;
-parameter WaitingForEnterOrCtrlPlusValue=1;
-parameter WaitingFor6Key=2;
-parameter CheckKey=3;
-parameter OpenSafeguard=4;
-parameter NotValid=5;
+
+parameter System_waiting_for_valid =1;
+parameter Close_door_return=2;
+parameter Check_Door_status=3;
+parameter Open_door_before_moving=4;
+parameter Close_door=5;
+parameter Move=6;
+parameter Floor_reached=7;
+reg [2:0] NF = 0;
 always @(posedge sys_clk)
 
 begin
 if(sys_rst)
 begin
-    rst_5<=0;
+    current_floor<=0;
+    NF<=0;
+    state<=System_waiting_for_valid;
+    rst_1<=0;
     rst_2<=0;
-    rst_8<=0;
-    counter <= 0;
-    Safeguard_status <=0; //sagefuard is closed = 0
+    rst_4<=0;
+    Door_status <=0; //open is 0
 end
 else
 	case(state)
 
-	WaitingForEnterOrCtrlPlusValue: //1
+	System_waiting_for_valid: //1
         begin 
-        Safeguard_status <= 0;
-        if(EnterPress || CtrlPress)
+        rst_2<=1;
+        if(!valid)
          begin
-            state<= WaitingFor6Key;
-        end
-        else  
-        begin
-            state<= WaitingForEnterOrCtrlPlusValue;
-        end
-    end
-
-    WaitingFor6Key: begin //2
-        rst_2<=1; 
-        if(counter == 6) 
-        begin //receive next values
-            counter <=0; //reset the counter since we dont need it anymore
             rst_2<=0;
-            state<= CheckKey
+            NF<=SW;
+            state<= Check_Door_status;
         end
-        else if(counter != 6 && !flag_2s)
+        else if(flag_2s) 
         begin
-            counter<= counter+1;
-        end
-        else if(flag_2s) //took more than 2 seconds between each input...
-        begin
-            rst_2<=0;
-            state<= WaitingForEnterOrCtrlPlusValue;
+            state<= Close_door_return;
         end
     end
 
-    CheckKey: begin //3
-            if(input_seq == password1 || input_seq == password2 || input_seq == password3 || input_seq == password4 || input_seq == password5)
-            begin
-            Safeguard_status = 1; //Open the safeguard
-            state<= OpenSafeguard;
-            end
-            else
-            state<= NotValid;
-    end
-
-    OpenSafeguard: begin //4
-        rst_8<=1; 
-        if (flag_8s)
+    Check_Door_status: begin //3
+        rst_1<=1; 
+        if(Door_status == 0 && flag_1s) 
+        begin // door open, close it
+            rst_1<=0;
+            state<= Close_door;
+        end
+        else if(Door_status == 1 && flag_1s)//door closed, open it
         begin
-        rst_8 <=0;
-        state<= WaitingForEnterOrCtrlPlusValue;
+            rst_1<=0;
+            state<= Open_door_before_moving;
         end
     end
 
-    NotValid: begin //5
-    rst_5<=1;
-    Safeguard_status <= 2; //Wrong password...
-    if(flag_5s)
+    Close_door_return: begin //2
+        rst_1<=1; 
+        if(flag_1s)
+        begin
+            rst_1<=0;
+            state<= System_waiting_for_valid;
+            Door_status <= 1;
+        end
+    end
+
+    Move: begin //6
+        rst_4<=1; 
+        if(current_floor>NF)
+        begin
+            if(flag_4s)
+            current_floor<=current_floor-1;
+        end
+        else if(current_floor<NF)
+        begin
+            if(flag_4s)
+            current_floor<=current_floor+1;
+        end
+        else 
+        begin
+            rst_4<=0; 
+            state<= Floor_reached;
+        end
+    end
+
+    Open_door_before_moving: begin //4
+    rst_1<=1;
+    if(flag_1s)
     begin
-        rst_5<=0;
-        state<= WaitingForEnterOrCtrlPlusValue;
+        rst_1<=0;
+        state<= Close_door;
+        Door_status <= 0;
     end
     end
 
+    Floor_reached: begin //7
+    rst_1<=1;
+    if(flag_1s)
+    begin
+        rst_1<=0;
+        state<= System_waiting_for_valid;
+        Door_status <= 0;
+    end
+    end
+
+    Close_door: begin //5
+    rst_1<=1;
+    if(flag_1s)
+    begin
+        rst_1<=0;
+        state<= Move;
+        Door_status <= 1;
+    end
+    end
     endcase
     end
 endmodule
